87654cf8274358c74d0cd8bc5c8cae829b3c73c8
Matthew Wang <matt@matthewwang.me>
2019-08-07 20:24:57 -0700

    adds question 2

--- server.py
+++ server.py
@@ -19,13 +19,27 @@ class Question1(Resource):
         args = q1_parser.parse_args()
         if (args['answer']):
             if args['answer'] == "42":
-                return {'response': 'congrats, you got the right answer! your next clue is at...'}
+                return {'response': 'congrats, you got the right answer! your next clue is <a href="https://whatthefatdog.com/mastering-physics/">here</a>'}
+            else:
+                return {'response': 'nice try, but that wasn\'t quite right. keep on thinking!!!'}
+        else:
+            return usageMessage
+            
+class Question2(Resource):
+    def get(self):
+        return usageMessage
+    def post(self):
+        args = q1_parser.parse_args()
+        if (args['answer']):
+            if args['answer'] == "42":
+                return {'response': 'nice, that\'s correct! but, in order to apply your answer, you\'ll need to look around you. and remember, think of the time!'}
             else:
                 return {'response': 'nice try, but that wasn\'t quite right. keep on thinking!!!'}
         else:
             return usageMessage
 
 api.add_resource(Question1, '/switch')
+api.add_resource(Question2, '/mastering-physics')
 
 if __name__ == '__main__':
     app.run(debug=True)
